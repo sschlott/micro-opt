8b41ad7477bed6f8a9dc6dd568396fd84cbc6acd
vagrant <schlotts@reed.edu>
2018-12-11 05:50:26 +0000

    working atoi for 5 digit number

--- test.c
+++ test.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
+#include <math.h>
+const int ONES = 1;
+const int TENS = 10;
+const int HUNDREDS = 100; 
+const int THOUSANDS = 1000;
+const int TTHOUS = 10000;
 int convert_all(char * word ) 
 {
-    int val = 0;
-    for (int u =0; u<6; u++)
-    {   
-       val = val + word[u];
-    }
+    int val = (word[0]-48)*TTHOUS + (word[1]-48)*THOUSANDS + (word[2]-48)*HUNDREDS + (word[3]-48)*TENS +(word[4]-48);
+
     return val;
 }
 int  main()
