65c4ec8acf4ac0ce05b967aa4d2fe5ab93bd52f0
sschlott <43354996+sschlott@users.noreply.github.com>
2018-12-10 22:00:12 -0800

    added clearer comments

--- test.c
+++ test.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-const int ONES = 1;
 const int TENS = 10;
 const int HUNDREDS = 100; 
 const int THOUSANDS = 1000;
@@ -8,7 +7,8 @@ const int TTHOUS = 10000;
 int convert_all(char * word ) 
 {
     int val = (word[0]-48)*TTHOUS + (word[1]-48)*THOUSANDS + (word[2]-48)*HUNDREDS + (word[3]-48)*TENS +(word[4]-48);
-
+    //Each index of the word is converted from ascii int to the number it reps (-48)
+    //Then scaled depending on its actual magnitude in the given number
     return val;
 }
 int  main()
