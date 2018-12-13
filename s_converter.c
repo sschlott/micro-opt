e5e8f01c0a1144a2c315ee1f948968f702402e00
vagrant <schlotts@reed.edu>
2018-12-13 06:49:51 +0000

    Cut off .0006s

--- s_converter.c
+++ s_converter.c
@@ -6,13 +6,11 @@
 //Bitwise conversions consistently slower. Tried with just on len 3, 4, 5 etc., always lost .001s+ 
 //i declared outside the loop harms performance (.002s slower)
 //Couldn't get cases to work in the manner I use if/else right now
+//Best run time overall: 0.003686s after changing multiplication process to smaller amounts at a time
 #include "converter.h"
 #include <stdlib.h>
 #include <stdio.h>
 const int TENS = 10;
-const int HUNDREDS = 100;
-const int THOUSANDS = 1000;
-const int TTHOUS = 10000;
 void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 {
     for (unsigned i = 0; i < nlines; ++i) 
@@ -23,18 +21,18 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 		if (word[3]!= '\0')
 		{
 		    
-		    nums[i]= (word[0]-48)*TTHOUS + (word[1]-48)*THOUSANDS + (word[2]-48)*HUNDREDS +(word[3]-48)*TENS + (word[4]-48);
+		    nums[i]= ((((word[0]-48)*TENS + (word[1]-48))*TENS + (word[2]-48))*TENS +(word[3]-48))*TENS + (word[4]-48);
 		}
 		else 
 		{
 
-		    nums[i]= (word[0]-48)*HUNDREDS + (word[1]-48)*TENS +(word[2]-48);
+		    nums[i]= ((word[0]-48)*TENS + (word[1]-48))*TENS +(word[2]-48);
 		}
 	}
 	else 
 	{
 		
-		nums[i]= (word[0]-48)*THOUSANDS + (word[1]-48)*HUNDREDS + (word[2]-48)*TENS +(word[3]-48);
+		nums[i]= (((word[0]-48)*TENS + (word[1]-48))*TENS + (word[2]-48))*TENS +(word[3]-48);
 	}
     }
 }
